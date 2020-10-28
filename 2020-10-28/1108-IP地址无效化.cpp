429e9dc5798f84c37a132eda4f3f8f11dbf56fd9
tabghu <1538546920@qq.com>
2020-10-28 09:29:46 +0800

    LeetCode

---
+++ 2020-10-28/1108-IP地址无效化.cpp
@@ -0,0 +1,31 @@
+//1108. IP 地址无效化
+//给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
+//
+//所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+//
+//
+//
+//示例 1：
+//
+//输入：address = "1.1.1.1"
+//输出："1[.]1[.]1[.]1"
+//示例 2：
+//
+//输入：address = "255.100.50.0"
+//输出："255[.]100[.]50[.]0"
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string str = "";
+        for (int i = 0; i < address.size(); i++)
+        {
+            if (address[i] == '.')
+                str += "[.]";
+            else
+            {
+                str += address[i];
+            }
+        }
+        return str;
+    }
+};
